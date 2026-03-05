25d6fee5d1ea356bdeb25405a6f6bda860bcbbd9
KaveriKadwadkar <kadwadkarkaveri28@gmail.com>
2026-03-05 15:05:39 +0530

    40 commit

--- api/admin/dashboard.php
+++ api/admin/dashboard.php
@@ -4,8 +4,8 @@
    IMPORT REQUIRED FILES
 ===================================== */
 
-require_once(__DIR__ . "../config.php");
-require_once(__DIR__ . "../api_guard.php");
+require_once(__DIR__ . "/../config.php");
+require_once(__DIR__ . "/../api_guard.php");
 require_once("../cors.php");
 /* =====================================
    SET RESPONSE TYPE
