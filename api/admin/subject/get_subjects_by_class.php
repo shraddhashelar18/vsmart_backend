aadcecb1fed12999a017f7ba00189a74ec7fb572
KaveriKadwadkar <kadwadkarkaveri28@gmail.com>
2026-03-06 11:24:52 +0530

    66th commit

--- api/admin/subject/get_subjects_by_class.php
+++ api/admin/subject/get_subjects_by_class.php
@@ -1,5 +1,5 @@
 <?php
-
+//add teacher.php
 require_once("../../config.php");
 require_once("../../api_guard.php");
 require_once("../../cors.php");
--- api/admin/teachers/add_teacher.php
+++ api/admin/teachers/add_teacher.php
@@ -1,5 +1,5 @@
 <?php
-
+//add teacher.php
 require_once(__DIR__ . "/../../config.php");
 require_once(__DIR__ . "/../../api_guard.php");
 
