a90c977597d06a58f35b840ad55397ee30bcd5c7
Harshitawav59 <harshitawavhal59@gmail.com>
2026-03-07 14:46:07 +0530

    69th commit

---
+++ api/classes.php
@@ -0,0 +1,10 @@
+<?php
+
+header("Content-Type: application/json");
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+echo json_encode([
+    "status" => true,
+    "department" => $data["department"] ?? "none"
+]);
